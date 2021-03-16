5810b224d575f99e9d36a517f48a50bd2ece092d
Aaron Sikes <aaron@sikes.io>
2021-03-16 11:14:10 -0400

    temp

--- gl.js
+++ gl.js
@@ -6,7 +6,38 @@ async function go() {
   let program = createProgram(gl, vert, frag);
 
   var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-  console.log(programAttributes(gl, program));
+  var positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  var positions = [
+    0, 0,
+    0, 0.5,
+    0.7, 0,
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  var vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  var size = 2;          // 2 components per iteration
+  var type = gl.FLOAT;   // the data is 32bit floats
+  var normalize = false; // don't normalize the data
+  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0;        // start at the beginning of the buffer
+  gl.vertexAttribPointer(
+    positionAttributeLocation, size, type, normalize, stride, offset);
+
+  // gl.viewport(0, 0, gl.canvas.width*window.devicePixelRatio, gl.canvas.height*window.devicePixelRatio);
+
+  gl.clearColor(0,0,0,0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.useProgram(program);
+  gl.bindVertexArray(vao);
+
+  var primitiveType = gl.TRIANGLES;
+  var offset = 0;
+  var count = 3;
+  gl.drawArrays(primitiveType, offset, count);
 }
 
 go();
